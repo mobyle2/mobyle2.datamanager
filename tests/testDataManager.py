c02a281d51f5dbfe06fa4cb743058e35a39c3daa
Olivier Sallou <olivier.sallou@irisa.fr>
2013-02-13 11:12:09 +0100

    add unit testing

---
+++ tests/testDataManager.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from pyramid import testing
+
+import pymongo
+import copy
+import os
+
+from bson.objectid import ObjectId
+
+import mobyle.common
+from mobyle.common.config import Config
+
+Config("development.ini")
+
+from mobyle.common import session
+
+base_url = "http://localhost:6543"
+
+import mobyle.common.connection
+mobyle.common.connection.init_mongo("mongodb://localhost/")
+import mobyle.data.manager.objectmanager
+from mobyle.data.manager.objectmanager import ObjectManager,FakeData
+
+class DataManagerTest(unittest.TestCase):
+
+        datadir = None
+
+        def setUp(self):
+            from webtest import TestApp
+            self.testapp = TestApp("config:development.ini", relative_to="./")
+            config = Config().config()
+            dirname, filename = os.path.split(os.path.abspath(__file__))
+            DataManagerTest.datadir = dirname + "/data"
+            config.set("app:main","store",DataManagerTest.datadir)
+            mobyle.common.session.register([FakeData])
+            datasets = mobyle.common.session.FakeData.find()
+            for data in datasets:
+                data.delete()
+
+
+        def test_add(self):
+            manager = ObjectManager()
+            id = manager.add("sample.py",__file__)
+            data = mobyle.common.session.FakeData.find_one({ '_id' : ObjectId(id) }) 
+            self.assertTrue(data is not None)
+            self.assertTrue(data['status']==ObjectManager.QUEUED)
+
+
+	def test_store(self):
+            manager = ObjectManager()
+            id = manager.store('sample.py',__file__,{})
+            data = mobyle.common.session.FakeData.find_one({ '_id' : ObjectId(id) })
+            self.assertTrue(data is not None)
+            self.assertTrue(data['status']==ObjectManager.DOWNLOADED)
+            self.assertTrue(os.path.exists(DataManagerTest.datadir+"/pairtree_root/"+data['path']))
+
+	def test_update(self):
+            manager = ObjectManager()
+            id = manager.add("sample.py",__file__)
+            manager.update(ObjectManager.ERROR,{ "id" : id})
+            data = mobyle.common.session.FakeData.find_one({ '_id' : ObjectId(id) })
+            self.assertTrue(data is not None)
+            self.assertTrue(data['status']==ObjectManager.ERROR)
+            
+
