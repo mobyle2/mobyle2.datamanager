dfc54ecb478857a1dfc4120ed72d7894ef0f8e17
Olivier Sallou <olivier.sallou@irisa.fr>
2013-12-03 18:03:44 +0100

    fix ftp after objectmanager modifications in mobyle.lib

--- mobyle/data/ftp/mobylefilesystem.py
+++ mobyle/data/ftp/mobylefilesystem.py
@@ -46,7 +46,7 @@ class MobyleFileSystem(AbstractedFS):
         if path == '/':
             path = u(path)
         self._cwd = u(self.fs2ftp(path))
-        logging.warn("chdir to " + self._cwd)
+        logging.debug("chdir to " + self._cwd)
 
     def mkdir(self, path):
         # Not possible
@@ -74,6 +74,9 @@ class MobyleFileSystem(AbstractedFS):
             # User project
             elif len(elts) == 2:
                 return True
+            # Dataset directory
+            elif len(elts) == 3:
+                return True
             # Next, should check if a sub dataset
         except Exception:
             return False
@@ -86,7 +89,7 @@ class MobyleFileSystem(AbstractedFS):
         logging.debug("cur dir is " + self._cwd)
         logging.debug("get_list_dir: " + path)
         if self.isdir(path):
-            logging.warn("list a directory")
+            logging.debug("list a directory")
             listing = self.listdir(path)
             try:
                 listing.sort()
@@ -115,15 +118,27 @@ class MobyleFileSystem(AbstractedFS):
         logging.debug("listdir: path= " + str(elts))
         if path == '/':
             # Root dir, list projects
-            projects = connection.Project.find({"users": {"$elemMatch": {'user.$id': ObjectId(self._uid)}}})
+            projects = connection.Project.find({"users": {"$elemMatch": {'user': ObjectId(self._uid)}}})
             for project in projects:
-                files.append(project)
+                files.append({'type' : 'project', 'elt' : project})
         else:
-            project = elts[1].split('_')
-            projectdata = connection.ProjectData.find({"project": ObjectId(project[0])})
-            for data in projectdata:
-                files.append(data)
-        logging.warn("list files " + str(files))
+            if len(elts)==2:
+                project = elts[1].split('_')
+                projectdata = connection.ProjectData.find({"project": ObjectId(project[0])})
+                for data in projectdata:
+                    files.append({'type' : 'datasets', 'elt' : data})
+            else:
+                dataset = elts[2].split('_')
+                dataset = connection.ProjectData.fetch_one({"_id": ObjectId(dataset[0])})
+                if 'value' in dataset['data']:
+                    for subdata in dataset['data']['value']:
+                        files.append({'type' : 'files', 'elt': { '_id':
+                        str(dataset['_id']), 'name' : subdata['path']}})
+                else:
+                    files.append({'type' : 'files', 'elt': { '_id' :
+                    str(dataset['_id']), 'name' : dataset['data']['path']}})
+
+        logging.debug("list files " + str(files))
         return files
 
     def getsize(self, path):
@@ -152,12 +167,11 @@ class MobyleFileSystem(AbstractedFS):
 
     def open(self, filename, mode):
         """Open a file returning its handler."""
-        logging.debug('open file ' + filename)
         paths = filename.split('/')
-        filename = paths[2]
-        filename = filename.split('_')[0]
-        mngr = ObjectManager()
-        filename = mngr.get_file_path(filename)
+        filename = paths[3]
+        datasetid = filename.split('_')[0]
+        filename = filename.split('_')[1]
+        filename = os.path.join(ObjectManager.get_file_path(datasetid),filename)
         logging.debug("## open " + filename)
         return open(filename, mode)
 
@@ -190,21 +204,24 @@ class MobyleFileSystem(AbstractedFS):
         #readlink = getattr(self, 'readlink', None)
         now = time.time()
         for basename in listing:
-            if not isinstance(basename, Project):
+            if basename['type'] is 'files':
                 try:
                     mngr = ObjectManager()
-                    st = self.lstat(mngr.get_file_path(str(basename['_id'])))
+                    st = self.lstat(os.path.join(mngr.get_file_path(
+                    basename['elt']['_id']), basename['elt']['name']))
+                    import stat
+                    size = st[stat.ST_SIZE]
                 except (OSError, FilesystemError):
                     if ignore_err:
                         continue
                     raise
-                size = basename['data']['size']  # file size
+                #size = basename['data']['size']  # file size
                 perms = _filemode(st.st_mode)  # permissions
-            else:
+            if basename['type'] is 'project' or basename['type'] is 'datasets':
                 # This is a project, fake a directory
                 size = 0
                 st = self.lstat(os.path.realpath(__file__))
-                basename['_id'] = str(basename['_id'])
+                basename['_id'] = str(basename['elt']['_id'])
                 perms = "drwxrwxrwx"
             nlinks = st.st_nlink  # number of links to inode
             if not nlinks:  # non-posix system, let's use a bogus value
@@ -236,6 +253,6 @@ class MobyleFileSystem(AbstractedFS):
             # formatting is matched with proftpd ls output
             line = "%s %3s %-8s %-8s %8s %s %s\r\n" % \
                 (perms, nlinks, uname, gname, size, mtimestr,
-                    str(basename['_id']) + "_" + basename['name'])
+                    str(basename['elt']['_id']) + "_" + basename['elt']['name'])
             logging.debug("out list: " + line)
             yield line.encode('utf8', self.cmd_channel.unicode_errors)
